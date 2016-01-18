eeea2e4aea02c5bc5c335f8b59cc7cb8bd3abb52
Thibaut Crenn <crenn@freshplanet.com>
2016-01-18 17:30:31 -0500

    adds android support for push notif tracking

--- android/src/com/freshplanet/nativeExtensions/StoreNotifTrackingInfo.java
+++ android/src/com/freshplanet/nativeExtensions/StoreNotifTrackingInfo.java
@@ -1,18 +1,66 @@
 package com.freshplanet.nativeExtensions;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREInvalidObjectException;
 import com.adobe.fre.FREObject;
+import com.adobe.fre.FRETypeMismatchException;
+import com.adobe.fre.FREWrongThreadException;
 
 public class StoreNotifTrackingInfo implements FREFunction {
 
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) {
-		// TODO Auto-generated method stub
 		
-		// TODO
+		Extension.log("start storing notif tracking");
+
+		if (arg1.length > 0)
+		{
+			String url = null;
+			try {
+				url = arg1[0].getAsString();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (FRETypeMismatchException e) {
+				e.printStackTrace();
+			} catch (FREInvalidObjectException e) {
+				e.printStackTrace();
+			} catch (FREWrongThreadException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			if (url != null) {
+				storeUrl(arg0.getActivity(), url);
+			} else
+			{
+				Extension.log("url tracking is null");
+			}
+			
+		} else
+		{
+			Extension.log("no arguments providing to store tracking url");
+		}
 		
 		return null;
 	}
 
+	private void storeUrl(Activity act, String url) {
+		
+		// We need an Editor object to make preference changes.
+		// All objects are from android.context.Context
+		SharedPreferences settings = act.getSharedPreferences(Extension.PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(Extension.PREFS_KEY, url);
+	
+	    // Commit the edits!
+		editor.commit();
+
+	}
+	
+	
 }
